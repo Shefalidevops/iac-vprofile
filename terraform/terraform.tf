ab2549f85b217bec50db13adbc74329e981d9f93
Shefalidevops <shefali25yt@gmail>
2025-02-12 12:50:08 -0500

    all version changes

--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -2,27 +2,27 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.25.0"
+      version = "~> 5.86.1"
     }
 
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.3"
     }
 
     tls = {
       source  = "hashicorp/tls"
-      version = "~> 4.0.4"
+      version = "~> 4.0.6"
     }
 
     cloudinit = {
       source  = "hashicorp/cloudinit"
-      version = "~> 2.3.2"
+      version = "~> 2.3.6-alpha1"
     }
 
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 2.23.0"
+      version = "~> 2.35.1"
     }
   }
 
@@ -32,7 +32,7 @@ terraform {
     region = "us-east-1"
   }
 
-  required_version = "~> 	1.10"
+  required_version = "~> 	1.10.4"
 }
 ##
 ##
